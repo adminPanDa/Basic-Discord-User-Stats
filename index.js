4b021de92b6b844dff718e8a04caece1a198793d
adminPanDa <qwer24069@gmail.com>
2018-04-09 14:40:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on( 'ready', () => {
 	console.log( `Logged in as ${ client.user.tag }!` );
 } );
 
-client.login( process.env.DISCORD_TOKEN );
+client.login(process.env.BOT_TOKEN);
 
 //  //  //  Express
 
